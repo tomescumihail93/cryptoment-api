8b740758ca08c0225da5ccc4dd51a326ae8dde40
Tomescu Mihail <tomescumihail93@gmail.com>
2018-01-13 12:53:44 +0100

    new test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var cfenv = require('cfenv');
 const app = express();
 
 
-app.get('/test', (req, res) => res.send('Hello World!'))
+app.get('/api/test', (req, res) => res.send('Hello World!'))
 
 var appEnv = cfenv.getAppEnv();
 app.listen(appEnv.port, () => console.log('Example app listening on port 3000!'))
