fa62e8f2f4b12d603baaf49ce6976d6314111843
Tomescu Mihail <tomescumihail93@gmail.com>
2018-01-17 12:45:18 +0100

    removed

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-var cfenv = require('cfenv');
-const app = express();
-
-
-app.get('/api/test', (req, res) => res.send('Hello World!'))
-
-var appEnv = cfenv.getAppEnv();
-app.listen(appEnv.port, () => console.log('Example app listening on port 3000!'))
