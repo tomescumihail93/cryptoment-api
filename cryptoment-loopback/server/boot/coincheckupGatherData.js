074040fcdda751d086dc0b799a537aaf2a83baca
Michel-Daniel COJOCARU (25559) <michel.cojocaru@stud.acs.upb.ro>
2018-01-28 15:32:35 +0100

    Added coincheckup DB updater (coins_db - push, investment/predictions/score - update) bug fixed: investment model ("symbol" field was null everytime) no more... future work: deploy independent scripts as apps on bluemix (+ RMI to the apps from loopback)

--- cryptoment-loopback/server/boot/coincheckupGatherData.js
+++ cryptoment-loopback/server/boot/coincheckupGatherData.js
@@ -4,7 +4,7 @@
  * */
 
 //careful about changing the next two variables standard 3600 & 0.2
-const samplingInterval = 1800;//seconds
+const samplingInterval = 120;//seconds
 //const waitAfterDBinit = 30;//seconds
 const depositDataInterval = 0.2;//seconds
 
@@ -26,16 +26,24 @@ var models = {
 
 };
 
+//limit the amount of entities populating/updating the DBs
+var hasLimit = true;
+//how many entities populating/updating the DBs
+var limit = 100;
+
+//date object to be used for current time acquisition
+var date = new Date();
+
 //if this is false supply only updates if it has a new value different from null
 const keepDuplicateData = false;
 
 
 //toggler for database initital population process (beware it overwrites all data in db)
-var isDbPopulated = false;
+var isDbPopulated = true;
 
 //first digit is "5" , "6" , "7" , "8" for coincheckup coin, investment, prediction, score model in this order.
 var initial_id = 913112091000832;
-
+/*
 //investment entity
 class Investment{
 
@@ -238,7 +246,7 @@ class Coin{
     }
 
 }
-
+*/
 Array.prototype.delayedForEach = function(callback, timeout, thisArg){
     var i = 0,
         l = this.length,
@@ -250,26 +258,13 @@ Array.prototype.delayedForEach = function(callback, timeout, thisArg){
     caller();
 };
 
-function getCoinBySymbol(symbol,coinList){
-    var ret_coin = null;
-
-    coinList.forEach(function(coin){
-        if(coin.symbol == symbol){
-            ret_coin = coin;
-        }
-    });
-
-    return ret_coin;
-}
-
 function run() {
 
     if (!isDbPopulated) {
         initializeDB();
     }
 
-    requestFromDB();
-    //setInterval(requestFromDB, samplingInterval * 1000);//
+    setInterval(requestFromDB, samplingInterval * 1000);//
 
 }
 
@@ -289,17 +284,14 @@ function requestFromDB(){
     request(options, function (error, response, body) {
         if (error) return console.error('Failed: %s', error.message);
         //console.log('Success: ', body);
-
         var remoteCoinList = JSON.parse(body);
-
         mergeAndSend(remoteCoinList);
     });
 }
 
-function setOptions(coin, key ,reserve_id){
+function setOptions(coinHistory, coin, key ,reserve_id, update){
 
     var var_id = parseInt(key.toString() + reserve_id.toString());
-
     var options = {
         method: 'PUT',
         url: 'https://cryptoment-api.mybluemix.net/api/coincheckup_' + models[key] + '_models',
@@ -312,24 +304,49 @@ function setOptions(coin, key ,reserve_id){
         json: true
     };
 
+    date = new Date();
+
     if(key == 5){
-        options.body = {
-            id: var_id.toString(),
-            availableSupply: [coin["available_supply"]],
-            lastUpdated: [coin["last_updated"]],
-            marketCapUSD: [coin["market_cap_usd"]],
-            maxSupply: [coin["max_supply"]],
-            name: coin["name"],
-            percentChange1h: coin["percent_change_1h"],
-            percentChange24h: coin["percent_change_24h"],
-            percentChange7d: coin["percent_change_7d"],
-            priceBTC: [coin["price_btc"]],
-            priceUSD: [coin["price_usd"]],
-            proofType: coin["market"] != null ? coin["market"]["proof_type"] : "",
-            rank: coin["rank"],
-            symbol: coin["symbol"],
-            totalSupply: [coin["total_supply"]],
-            volume24h: [coin["24h_volume_usd"]]
+        if(!update) {
+            options.body = {
+                id: var_id.toString(),
+                availableSupply: [coin["available_supply"]],
+                lastUpdated: [date.getTime().toString()],//[coin["last_updated"]],
+                marketCapUSD: [coin["market_cap_usd"]],
+                maxSupply: [coin["max_supply"]],
+                name: coin["name"],
+                percentChange1h: coin["percent_change_1h"],
+                percentChange24h: coin["percent_change_24h"],
+                percentChange7d: coin["percent_change_7d"],
+                priceBTC: [coin["price_btc"]],
+                priceUSD: [coin["price_usd"]],
+                proofType: coin["market"] != null ? coin["market"]["proof_type"] : "",
+                rank: coin["rank"],
+                symbol: coin["symbol"],
+                totalSupply: [coin["total_supply"]],
+                volume24h: [coin["24h_volume_usd"]]
+            }
+        }else if(update && coinHistory != null){
+            options.body = {
+                id: var_id.toString(),
+                name: coin["name"],
+                percentChange1h: coin["percent_change_1h"],
+                percentChange24h: coin["percent_change_24h"],
+                percentChange7d: coin["percent_change_7d"],
+                rank: coin["rank"],
+                symbol: coin["symbol"],
+                proofType: coin["market"] != null ? coin["market"]["proof_type"] : "",
+
+                priceUSD: mergeHistoricalWithCurrent(coinHistory["priceUSD"],coin["price_usd"],true),
+                priceBTC: mergeHistoricalWithCurrent(coinHistory["priceBTC"],coin["price_btc"],true),
+                lastUpdated: mergeHistoricalWithCurrent(coinHistory["lastUpdated"],date.getTime().toString()/*coin["last_updated"]*/,true),
+                marketCapUSD: mergeHistoricalWithCurrent(coinHistory["marketCapUSD"],coin["market_cap_usd"],true),
+                volume24h: mergeHistoricalWithCurrent(coinHistory["volume24h"],coin["24h_volume_usd"],true),
+                maxSupply: mergeHistoricalWithCurrent(coinHistory["maxSupply"],coin["max_supply"],keepDuplicateData),//keepDuplicateData
+                availableSupply: mergeHistoricalWithCurrent(coinHistory["availableSupply"],coin["available_supply"],keepDuplicateData),
+                totalSupply: mergeHistoricalWithCurrent(coinHistory["totalSupply"],coin["total_supply"],keepDuplicateData)
+
+            }
         }
     }else if(key == 6){
         options.body = {
@@ -337,6 +354,7 @@ function setOptions(coin, key ,reserve_id){
             name: coin["name"],
             rank: coin["rank"],
             symbol: coin["symbol"],
+            lastUpdated: date.getTime().toString(),
             averageMarketCap1m: coin["investment"] != null ? coin["investment"]["average_mktcap_1m"] : "",
             averageMothlyRoi1y: coin["investment"] != null ? coin["investment"]["average_monthly_roi_1y"] : "",
             averageVolume1m: coin["investment"] != null ? coin["investment"]["average_volume_1m"] : "",
@@ -361,6 +379,7 @@ function setOptions(coin, key ,reserve_id){
             name: coin["name"],
             rank: coin["rank"],
             symbol: coin["symbol"],
+            lastUpdated: date.getTime().toString(),
             growthBtc: coin["predictions"] != null ? coin["predictions"]["growth_btc"] : "",
             growthOthers: coin["predictions"] != null ? coin["predictions"]["growth_others"] : "",
             worldMoney: coin["predictions"] != null ? coin["predictions"]["worldMoney"] : ""
@@ -371,6 +390,7 @@ function setOptions(coin, key ,reserve_id){
             name: coin["name"],
             rank: coin["rank"],
             symbol: coin["symbol"],
+            lastUpdated: date.getTime().toString(),
             advisorsScore: coin["scores"] != null ? coin["scores"]["D"] : "",//
             businessScore: coin["scores"] != null ? coin["scores"]["H"] : "",//
             coinScore: coin["scores"] != null ? coin["scores"]["G"] : "",//
@@ -388,35 +408,29 @@ function setOptions(coin, key ,reserve_id){
 }
 
 var initializeDB = function() {
-
     request("https://coincheckup.com/data/prod/201801261131/coins.json", function (error, response, body) {
-
         if (!error && response.statusCode == 200) {
-
             var reserve_id = initial_id;
-
             var res = JSON.parse(body);
 
-            res.delayedForEach(function (coin) {
+            res.delayedForEach(function (coin, index) {
 
+                if(hasLimit && (index >= limit)){
+                    return;
+                }
                 reserve_id++;
 
-                Object.keys(models).delayedForEach(function (key/*, indexKeys, arrayKeys*/) {
-
-                    var options = setOptions(coin, key , reserve_id);
-
+                Object.keys(models).delayedForEach(function (key) {
+                    var options = setOptions(null, coin, key , reserve_id, false);
                     throttledRequest(options, function (error, response, body) {
                         if (error){
                             console.error('Failed: %s', error.message);
                         }
                         console.log('Success:\n', body);
                     });
-
                 }, depositDataInterval * 1000);
-
             }, 4.5 * depositDataInterval * 1000);
         }
-
     });
 
     //isDbPopulated = true;
@@ -425,17 +439,6 @@ var initializeDB = function() {
 };
 
 function mergeHistoricalWithCurrent(oldData, currentData, keepNulls){
-
-/*
-    console.log("------------------------------------");
-    console.log("oldData: " + oldData);
-    console.log("JSON.parse(oldData): " + JSON.parse(oldData));
-    console.log("typeof(oldData): " + typeof(oldData));
-
-    console.log("currentData: " + currentData);
-    console.log("typeof(currentData): " + typeof(currentData));
-    console.log("------------------------------------");
-*/
 
     var list = oldData;
 
@@ -473,87 +476,37 @@ var mergeAndSend = function(coinList) {
 
     //update historical data with fresh data
     request("https://coincheckup.com/data/prod/201801261131/coins.json", function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                var res = JSON.parse(body);
+        if (!error && response.statusCode == 200) {
+            var res = JSON.parse(body);
+            var reserve_id = initial_id;
 
-                res.delayedForEach(function (freshCoin) {
+            res.delayedForEach(function (freshCoin, index) {
 
-                    var coinHistory = null;
-                    for (var i = 0; i < coinList.length ; i++){
+                if(hasLimit && (index >= limit)){
+                    return;
+                }
 
-                        if(String(coinList[i]["symbol"]) == freshCoin["symbol"]){
-                            coinHistory = coinList[i];
-                            break;
+                reserve_id++;
+
+                var coinHistory = null;
+                for (var i = 0; i < coinList.length ; i++){
+                    if(String(coinList[i]["symbol"]) == freshCoin["symbol"]){
+                        coinHistory = coinList[i];
+                        break;
+                    }
+                }
+                Object.keys(models).delayedForEach(function (key) {
+                    var options = setOptions(coinHistory, freshCoin, key, reserve_id, true);//update == true
+                    throttledRequest(options, function (error, response, body) {
+                        if (error) {
+                            console.error('Failed: %s', error.message);
                         }
-                    }
-
-                    //retrieve coin from local copy
-                    if (coinHistory != null && coinHistory != undefined) {
-                        //coinHistory.id = freshCoin["id"];
-                        //coinHistory.id = coinHistory.id;
-                        coinHistory.name = freshCoin["name"];
-                        coinHistory.percentChange1h = freshCoin["percent_change_1h"];
-                        coinHistory.percentChange24h = freshCoin["percent_change_24h"];
-                        coinHistory.percentChange7d = freshCoin["percent_change_7d"];
-                        coinHistory.rank = freshCoin["rank"];
-                        coinHistory.symbol = freshCoin["symbol"];
-                        coinHistory.proofType = freshCoin["market"] != null ? freshCoin["market"]["proof_type"] : "";
-
-                        coinHistory.priceUSD = mergeHistoricalWithCurrent(coinHistory["priceUSD"],freshCoin["price_usd"],true);
-                        coinHistory.priceBTC = mergeHistoricalWithCurrent(coinHistory["priceBTC"],freshCoin["price_btc"],true);
-                        coinHistory.lastUpdated = mergeHistoricalWithCurrent(coinHistory["lastUpdated"],freshCoin["last_updated"],true);
-                        coinHistory.marketCapUSD = mergeHistoricalWithCurrent(coinHistory["marketCapUSD"],freshCoin["market_cap_usd"],true);
-                        coinHistory.volume24h = mergeHistoricalWithCurrent(coinHistory["volume24h"],freshCoin["24h_volume_usd"],true);
-                        coinHistory.maxSupply = mergeHistoricalWithCurrent(coinHistory["maxSupply"],freshCoin["max_supply"],keepDuplicateData);//keepDuplicateData
-                        coinHistory.availableSupply = mergeHistoricalWithCurrent(coinHistory["availableSupply"],freshCoin["available_supply"],keepDuplicateData);
-                        coinHistory.totalSupply = mergeHistoricalWithCurrent(coinHistory["totalSupply"],freshCoin["total_supply"],keepDuplicateData);
-
-
-
-                        //update data to remote db
-                        var options = {
-                            method: 'PUT',
-                            url: 'https://cryptoment-api.mybluemix.net/api/coincheckup_coin_models',
-                            headers: {
-                                accept: 'application/json',
-                                'content-type': 'application/json',
-                                'x-ibm-client-secret': 'e399a9e4ba39d2941cb14875f7545e418a96ca905c7b7915f6199b87adde9467',
-                                'x-ibm-client-id': '62384ebd-6575-4fa6-b4ef-b1886c6c7d26-bluemix'
-                            },
-                            body: {
-                                "id": coinHistory.id,
-                                "availableSupply": coinHistory.availableSupply,
-                                "lastUpdated": coinHistory.lastUpdated,
-                                "marketCapUSD": coinHistory.marketCapUSD,
-                                "maxSupply": coinHistory.maxSupply,
-                                "name": coinHistory.name,
-                                "percentChange1h": coinHistory.percentChange1h,
-                                "percentChange24h": coinHistory.percentChange24h,
-                                "percentChange7d": coinHistory.percentChange7d,
-                                "priceBTC": coinHistory.priceBTC,
-                                "priceUSD": coinHistory.priceUSD,
-                                "rank": coinHistory.rank,
-                                "symbol": coinHistory.symbol,
-                                "proofType": coinHistory.proofType,
-                                "totalSupply": coinHistory.totalSupply,
-                                "volume24h": coinHistory.volume24h
-                            },
-                            json: true
-                        };
-
-                        throttledRequest(options, function (error, response, body) {
-                            if (error) return console.error('Failed: %s', error.message);
-                            console.log('Success: ', body);
-                        });
-
-                    }
-
+                        console.log('Success:\n', body);
+                    });
                 }, depositDataInterval * 1000);
-
-            }
-
-        });
-
+            }, 4.5 * depositDataInterval * 1000);
+        }
+    });
 };
 
 
